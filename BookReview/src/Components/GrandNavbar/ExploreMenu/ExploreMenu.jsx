3d805c52115b971178d1a8645106185d6d37d70c
Lynneruup <andreaslynnerup@outlook.dk>
2023-06-07 14:11:09 +0200

    GrandNavbar redesign

--- BookReview/src/Components/GrandNavbar/ExploreMenu/ExploreMenu.jsx
+++ BookReview/src/Components/GrandNavbar/ExploreMenu/ExploreMenu.jsx
@@ -5,39 +5,27 @@ const ExploreMenu = () => {
 
 	return (
 	<div className="menuContainer">
-		<ul>
-			<li className="menuItem">
-				<div>Membership</div>
-				<div className="dropDown">
-					<div className="dropDownHeader">Why a membership?
-					</div>
-					<div className="dropDownHeader">Advantages</div>
-				</div>
+		<ul className="actualList">
+			<li className="menuItem">Membership
+				<ul className="drop">
+					<li className="listItem">Advantages</li>
+					<li className="listItem">Student</li>
+					<li className="listItem">Events</li>
+				</ul>
 			</li>
-			<li className="menuItem">
-				<div>Bestsellers</div>
-				<div className="dropDown"></div>
-			</li>
-			<li className="menuItem">
-				<div>Fiction</div>
-				<div className="dropDown"></div>
-			</li>
-			<li className="menuItem">
-				<div>Non-fiction</div>
-				<div className="dropDown"></div>
-			</li>
-			<li className="menuItem">
-				<div>Academics</div>
-				<div className="dropDown"></div>
-			</li>
-			<li className="menuItem">
-				<div>Kids</div>
-				<div className="dropDown"></div>
-			</li>
-			<li className="menuItem">
-				<div>Explore</div>
-				<div className="dropDown"></div>
-			</li>
+{			<li className="menuItem">Fiction
+				<ul className="drop">
+					<li className="listItem">Advantages</li>
+					<li className="listItem">Student</li>
+					<li className="listItem">Events</li>
+				</ul>
+			</li>}
+{			<li className="menuItem">Non Fiction
+				<ul className="drop">
+					<li className="listItem">Advantages</li>
+					<li className="listItem">Student</li>
+				</ul>
+			</li>}
 		</ul>
 	</div>
 	)
